25970d83b718508662019a7aa6f8b3ffa1397aee
Van Hoan <vanhoanweb@gmail.com>
2017-09-08 11:37:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+( function( $ ) {
+	/* Responsive Menu */
+	$(".responsive-menu-icon").click(function() {
+		$(this).next(".nav-primary .menu").toggle();
+		$(this).toggleClass("btn-close");
+		$('.overlay').toggleClass('show');
+	});
+
+	$(window).resize(function() {
+		if(window.innerWidth > 767) {
+			$(".nav-primary .menu, nav .sub-menu").removeAttr("style");
+			$(".responsive-menu > .menu-item").removeClass("menu-open");
+		}
+	});
+
+	var isMobile = window.matchMedia("only screen and (max-width: 767px)");	
+	if (isMobile.matches) {
+		$(".site-header").addClass("fixed");
+	}
+
+	$(".responsive-menu > .menu-item").click(function(event) {
+		if (event.target !== this) return;
+		$(this).find(".sub-menu:first").slideToggle(function() {
+			$(this).parent().toggleClass("menu-open");
+		});
+	});
+	
+	/* Page scrolling feature - requires jQuery Easing plugin */
+	$('nav a, #home a').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+
+	/* Menu bar fixed on top when scrolled */
+	var isLaptop = window.matchMedia("only screen and (min-width: 1000px)");
+	if (isLaptop.matches) {
+		$(window).bind('scroll', function(event) {
+			if ($(window).scrollTop() > 50) {
+				$('.site-header').addClass('fixed');
+			} else {
+				$('.site-header').removeClass('fixed');
+			}
+		});
+	}	
+}( jQuery ) );
